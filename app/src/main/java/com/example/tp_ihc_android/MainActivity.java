8254c0455fce4714e25379efab137df288e97081
Gustavo Oliva Barnasque <gobarnasque@idejr.com.br>
2020-05-21 22:36:47 -0300

    Consegue fazer chamada rest pelo app

--- app/src/main/java/com/example/tp_ihc_android/MainActivity.java
+++ app/src/main/java/com/example/tp_ihc_android/MainActivity.java
@@ -2,54 +2,85 @@ package com.example.tp_ihc_android;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Debug;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class MainActivity<string> extends AppCompatActivity {
+
+    Button btnTeste;
+    TextView tvTeste;
+
+    RequestQueue queue;
+    String url = "https://dog.ceo/api/breeds/image/random/";
+    StringRequest stringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btnTeste = findViewById(R.id.btnTeste);
+        tvTeste = findViewById(R.id.tvTeste);
+
+        queue = Volley.newRequestQueue(this);
+        url = "https://dog.ceo/api/breeds/image/random";
+        // Request a string response from the provided URL.
+        stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String test = jsonObject.optString("message");
+                            tvTeste.setText(test);
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                            Log.wtf("erro", e.getMessage());
+                            Log.d("erro", e.getMessage());
+                            tvTeste.setText("deu ruim.");
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        tvTeste.setText("deu ruim.");
+                    }
+                });
+
+        btnTeste.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
